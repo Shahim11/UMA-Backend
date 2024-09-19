a389dab656334e457eabf0f07bdc446aa400b39a
Shahim Uddin <Shahim6872@gmail.com>
2024-09-19 18:21:20 +0600

    5th commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const authenticateToken = require('../middlewares/auth');
 
 // Import controllers
 const registerController = require('../controllers/registerController');
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authenticateToken = require('../middlewares/auth');
+const authenticateToken = require('../middlewares/authToken');
 
 // Import controllers
 const usersController = require('../controllers/usersController');
