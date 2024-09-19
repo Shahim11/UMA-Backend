b11e473a508e1f4e64d3430090ce6102751732a8
Shahim Uddin <Shahim6872@gmail.com>
2024-09-20 00:34:06 +0600

    5th commit

--- config/db.js
+++ config/db.js
@@ -5,11 +5,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    port: process.env.MYSQL_ADDON_PORT
 });
 
 connection.connect(err => {
