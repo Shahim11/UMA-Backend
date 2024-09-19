c5c9509ca4c86eced29cfa826fa7062d33824345
Shahim Uddin <Shahim6872@gmail.com>
2024-09-20 01:08:37 +0600

    5th commit

--- config/db.js
+++ config/db.js
@@ -8,8 +8,7 @@ const connection = mysql.createConnection({
     host: process.env.MYSQL_ADDON_HOST,
     user: process.env.MYSQL_ADDON_USER,
     password: process.env.MYSQL_ADDON_PASSWORD,
-    database: process.env.MYSQL_ADDON_DB,
-    port: process.env.MYSQL_ADDON_PORT
+    database: process.env.MYSQL_ADDON_DB
 });
 
 connection.connect(err => {
